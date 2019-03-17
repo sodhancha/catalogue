a9600f92145fb630b04a08872755bd17b03e66a4
kripal <kripalshrestha@hotmail.com>
2019-03-17 11:47:32 +0545

    final commit

--- config/data/database.sql
+++ config/data/database.sql
@@ -14,10 +14,10 @@ CREATE TABLE `products` (
 
 CREATE TABLE `users` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(50),
-  `email` varchar(256),
-  `password` varchar(2048),
+  `name` varchar(50) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(2048) NOT NULL,
   `created` datetime NOT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+); ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
