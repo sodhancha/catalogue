f398d6db716994ea6b6713239edb49c79441bd5a
kripal <kripalshrestha@hotmail.com>
2019-03-17 07:37:22 +0545

    validate token

---
+++ validate_token.php
@@ -0,0 +1,52 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost/catalogue/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+
+use \Firebase\JWT\JWT;
+
+$data = json_decode(file_get_contents("php://input"));
+$jwt  = isset($data->jwt) ? $data->jwt : "";
+
+// if jwt is not empty
+if($jwt){
+    // if decode succeed, show user details
+    try {
+        // decode jwt
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+
+        // set response code
+        http_response_code(200);
+
+        // show user details
+        echo json_encode(array(
+            "message" => "Access granted.",
+            "data" => $decoded->data
+        ));
+    }
+    // if decode fails, it means jwt is invalid
+    catch (Exception $e){
+
+        // set response code
+        http_response_code(401);
+
+        // tell the user access denied  & show error message
+        echo json_encode(array(
+            "message" => "Access denied.",
+            "error" => $e->getMessage()
+        ));
+    }
+}
+else
+{
+    http_response_code(401);
+    echo json_encode(array("message" => "Access denied."));
+}
