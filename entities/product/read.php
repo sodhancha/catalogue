3d42a5ac0ac8097edd0ea295b10fa77a91c221ec
kripal <kripalshrestha@hotmail.com>
2019-03-17 08:12:57 +0545

    validate token read

--- entities/product/read.php
+++ entities/product/read.php
@@ -5,39 +5,72 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once '../../config/dbClass.php';
 include_once '../../modals/product.php';
 
+// required to encode json web token
+include_once '../../config/core.php';
+include_once '../../libs/php-jwt-master/src/BeforeValidException.php';
+include_once '../../libs/php-jwt-master/src/ExpiredException.php';
+include_once '../../libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once '../../libs/php-jwt-master/src/JWT.php';
+
+use \Firebase\JWT\JWT;
+
 //database and product objects
 $database = new dbClass();
 $db       = $database->getConnection();
 $product  = new Product($db);
 
-$prods = $product->read();
-$num   = $prods->rowCount();
+$data = json_decode(file_get_contents("php://input"));
+$jwt  = isset($data->jwt) ? $data->jwt : "";
 
-if ($num > 0)
+if ($jwt)
 {
-    $products_arr            = [];
-    $products_arr["records"] = [];
-
-    while ($row = $prods->fetch(PDO::FETCH_ASSOC))
+    // if decode succeed, show user details
+    try
     {
-        extract($row);
+        // decode jwt
+        $decoded = JWT::decode($jwt, $key, [ 'HS256' ]);
 
-        $product_item = [
-            "id"          => $id,
-            "name"        => $name,
-            "description" => html_entity_decode($description),
-            "price"       => $price,
-            "quantity"    => $quantity,
-        ];
+        $prods = $product->read();
+        $num   = $prods->rowCount();
 
-        array_push($products_arr["records"], $product_item);
+        if ($num > 0)
+        {
+            $products_arr            = [];
+            $products_arr["records"] = [];
+
+            while ($row = $prods->fetch(PDO::FETCH_ASSOC))
+            {
+                extract($row);
+
+                $product_item = [
+                    "id"          => $id,
+                    "name"        => $name,
+                    "description" => html_entity_decode($description),
+                    "price"       => $price,
+                    "quantity"    => $quantity,
+                ];
+
+                array_push($products_arr["records"], $product_item);
+            }
+
+            http_response_code(200);
+            echo json_encode($products_arr);
+        }
+        else
+        {
+            http_response_code(404);
+            echo json_encode([ "message" => "No products found." ]);
+        }
+    } catch (Exception $e)
+    {
+
+        // set response code
+        http_response_code(401);
+
+        // show error message
+        echo json_encode([
+            "message" => "Access denied.",
+            "error"   => $e->getMessage()
+        ]);
     }
-
-    http_response_code(200);
-    echo json_encode($products_arr);
-}
-else
-{
-    http_response_code(404);
-    echo json_encode([ "message" => "No products found." ]);
 }
