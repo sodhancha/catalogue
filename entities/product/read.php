75589ba6525783bb239a9b518a9cf5837cf1791e
mac <sodhan@hitidesign.com.np>
2019-03-17 15:31:44 +0545

    Common header files added.

--- entities/product/read.php
+++ entities/product/read.php
@@ -1,16 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
-include_once '../../config/dbClass.php';
-include_once '../../modals/product.php';
-
-// required to encode json web token
-include_once '../../config/core.php';
-include_once '../../libs/php-jwt-master/src/BeforeValidException.php';
-include_once '../../libs/php-jwt-master/src/ExpiredException.php';
-include_once '../../libs/php-jwt-master/src/SignatureInvalidException.php';
-include_once '../../libs/php-jwt-master/src/JWT.php';
+include('common_header.php');
 
 use \Firebase\JWT\JWT;
 
--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
+<h1>Krpials Catalog</h1>
